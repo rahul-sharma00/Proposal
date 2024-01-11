20494e9ac623f1c459a6e7817582d7233d573af1
Rahul Sharma <131450041+rahul-sharma00@users.noreply.github.com>
2024-01-11 18:12:59 +0530

    Add files via upload

---
+++ Rahul.js
@@ -0,0 +1,12 @@
+const btn1=document.getElementById("batan");
+const btn2=document.getElementById('bt');
+
+
+btn1.addEventListener('mouseover',()=>{
+    window.alert("You broke my heart💔");
+});
+
+function fun(){
+    document.getElementById("gif").src="gif2.gif";
+}
+
